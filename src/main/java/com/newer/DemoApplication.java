4c5a9052e5bae369c08ccb314ccec5434c391131
kekesam02 <1571828260@qq.com>
2019-03-06 01:29:43 +0800

    h

--- src/main/java/com/newer/DemoApplication.java
+++ src/main/java/com/newer/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     //好哈
-    //嘻嘻嘻
+    //嘻嘻嘻嘿嘿嘿嘿
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
