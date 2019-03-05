1b7f361e80d1d18c209dcca84babfbad846903a8
kekesam02 <1571828260@qq.com>
2019-03-06 01:21:00 +0800

    项目初始化提交

--- src/main/java/com/newer/DemoApplication.java
+++ src/main/java/com/newer/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+    //好哈
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
